86276035e477b5229c5ed0201b14f0200b6c2753
dafedin <tm.i.sm.master@gmail.com>
2024-06-03 16:15:57 +0200

    dummy

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+""" Get .py file from .inp file automatically"""
 import os
 
 from ansys.mapdl import core as pymapdl
-from ansys.mapdl.core import launch_mapdl
 
-mapdl = launch_mapdl()
+mapdl = pymapdl.launch_mapdl()
 
 search_dir = os.path.join(os.path.dirname(__file__), "sources")
+
 search_path = os.path.join(search_dir, "beam.inp")
 
-pymapdl.convert_script(search_path)  # , pyscript)
+save_path = os.path.join(os.path.dirname(__file__), "tmp")
+
+print(save_path)
+
+pymapdl.convert_script(filename_in=search_path, filename_out=save_path)  # , pyscript)
 
 mapdl.exit()
