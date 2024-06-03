4cafe80aab6210792ef51bd0bddbb1bc82455973
dafedin <tm.i.sm.master@gmail.com>
2024-06-03 16:05:06 +0200

    autegeneration of py file from inp file

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from ansys.mapdl.core import launch_mapdl
 import os
+
+from ansys.mapdl import core as pymapdl
+from ansys.mapdl.core import launch_mapdl
 
 mapdl = launch_mapdl()
 
-print(mapdl)
+search_dir = os.path.join(os.path.dirname(__file__), "sources")
+search_path = os.path.join(search_dir, "beam.inp")
 
-project_path = os.getcwd()
-job_name = "user_job_name"
-
-print(job_name)
+pymapdl.convert_script(search_path)  # , pyscript)
 
 mapdl.exit()
